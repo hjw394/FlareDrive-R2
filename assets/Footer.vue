f422adb2f9ead8682d6f6e61f1a95bcaae1048b4
hjw394 <excit123@qq.com>
2025-12-04 21:58:18 +0800

    Update Footer.vue

--- assets/Footer.vue
+++ assets/Footer.vue
@@ -20,9 +20,9 @@
     </div>
 
     <div class="project-links">
-      <strong>项目:</strong><a href="https://github.com/willow-god/FlareDrive-R2" target="_blank"
+      <strong>项目:</strong><a href="https://github.com/hjw394/FlareDrive-R2" target="_blank"
         rel="noopener noreferrer">FlareDrive-R2</a>
-      <strong>作者:</strong><a href="https://github.com/willow-god" target="_blank"
+      <strong>作者:</strong><a href="https://github.com/hjw394" target="_blank"
         rel="noopener noreferrer">LiuShen</a>
       <strong>初始项目:</strong><a href="https://github.com/ljxi/Cloudflare-R2-oss" target="_blank"
         rel="noopener noreferrer">Cloudflare-R2-OSS</a>
@@ -42,7 +42,7 @@ export default {
     return {
       homeUrl: "https://www.liushen.fun/",
       blogUrl: "https://blog.liushen.fun/",
-      githubUrl: "https://github.com/willow-god",
+      githubUrl: "https://github.com/hjw394",
       emailUrl: "mailto:01@liushen.fun"
     };
   }
